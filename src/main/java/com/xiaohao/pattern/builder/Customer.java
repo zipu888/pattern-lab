de5aa71afa9136c250e50abad28f27f0321774a1
superhao <zipu888@aliyun.com>
2014-10-08 14:17:37 +0800

    modify

--- src/main/java/com/xiaohao/pattern/builder/Customer.java
+++ src/main/java/com/xiaohao/pattern/builder/Customer.java
@@ -2,6 +2,8 @@ package com.xiaohao.pattern.builder;
 
 /**
  * Created by xiaohao on 2014/10/8.
+ * Builder pattern is used when there are many steps to create an object.
+ * Factory pattern is used when the factory can easily create the entire object within one method call
  */
 public class Customer {
 
